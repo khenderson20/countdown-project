74b9e48efba86a89df108ec517c80d3389d63be4
Kevin <kevin.d.hende@gmail.com>
2021-11-03 17:57:20 -0500

    began styling the Event.vue component
    using vanilla CSS styling and Flexbox.

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,10 +1,48 @@
 <template>
-  <p>Hello Vue.JS!</p>
+  <article>
+    <div class="data">
+      <h3 class="name">Title</h3>
+      <p class="details">some text</p>
+    </div>
+    <div class="countdown">
+      <div class="remove_btn_wrapper">
+        <button class="remove_btn">&#10060;</button>
+      </div>
+      <p>
+        32
+        <br />
+        <small>days left</small>
+      </p>
+    </div>
+  </article>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Event",
+};
 </script>
 
-<style>
+<style scoped>
+article {
+  background: lightslategrey;
+  display: flex;
+  align-items: center;
+  border-radius: 1rem;
+  margin: 1rem 0;
+  padding: 0.2em 1rem;
+  color: whitesmoke;
+  font-weight: 300;
+}
+
+.data {
+  flex: 3 1 0;
+}
+
+.countdown {
+  flex: 1 1 0;
+  text-align: center;
+  font-size: 1.6rem;
+  border-left: 1px solid;
+}
 </style>
